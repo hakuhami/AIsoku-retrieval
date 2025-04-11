b30a6b47c6e32d06a72c8b02dc07909d26ba0130
hakuhami <15sen3haku@gmail.com>
2025-04-11 23:31:19 +0900

    FIREBASE_SERVICE_ACCOUNTのjsonを直接環境変数から取得できるように

--- components/firebase_utils.py
+++ components/firebase_utils.py
@@ -1,11 +1,20 @@
 import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+# def init_firebase():
+#     cred_path = os.environ.get("FIREBASE_SERVICE_ACCOUNT_PATH")
+#     if not firebase_admin._apps:
+#         cred = credentials.Certificate(cred_path)
+#         firebase_admin.initialize_app(cred)
+#     return firestore.client()
+
 def init_firebase():
-    cred_path = os.environ.get("FIREBASE_SERVICE_ACCOUNT_PATH")
+    cred_json = os.environ.get("FIREBASE_SERVICE_ACCOUNT_JSON")    
     if not firebase_admin._apps:
-        cred = credentials.Certificate(cred_path)
+        cred_dict = json.loads(cred_json)
+        cred = credentials.Certificate(cred_dict)
         firebase_admin.initialize_app(cred)
     return firestore.client()
 
